45ff2a34ad2cd0b49a834085f5035ce667de4b7a
alicjak1519 <48221385+alicjak1519@users.noreply.github.com>
2019-03-22 14:24:56 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -1,28 +1,37 @@
-# Write a module that will simulate autonomic car.
-# The simulation is event based, an example:
-# car1 = Car()
-# car1.act(event)
-# print(car1.wheel_angle, car1.speed)
-# where event can be anything you want, i.e. :
-# `('obstacle', 10)` where `10` is a duration (time) of the event.
-##The program should:
-# - act on the event
-# - print out current steering wheel angle, and speed
-# - run in infinite loop
-# - until user breaks the loop
+class Car():
 
-#The level of realism in simulation is of your choice, but more sophisticated solutions are better.
-#If you can thing of any other features, you can add them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
-    
+	def __init__(self):
+		print("START")
+		self.wheel_angle = 0
+		self.speed = 0
+	
+	def __del__(self):
+		print("STOP")
+
+	def act(self, event):
+		print("obstacle - 1, pedestrian - 2, turning - 3")
+		input(event)
+		
+		if event == 1:
+			self.wheel_angle += 90
+			self.speed = 10
+		elif event == 2:
+			self.speed = 0
+		else:
+			self.speed -= 5
+			self.wheel_angle += 90
+
+
+car1 = Car()
+
+
+for i in range(0,3):
+
+	print("obstacle - 1, pedestrian - 2, turning - 3")
+	event = input()
+	car1.act(event)
+	print("Actual speed is: {}".format(car1.speed))
+	print("Actual wheel angle is: {}".format(car1.wheel_angle))
+
+	
+
